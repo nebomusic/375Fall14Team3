f3fed1c4591ac137ae0a0c771aae5b4c1a6c062c
Jeremy Benson <jeremybenson18@marist.com>
2014-10-22 12:39:03 -0400

    add family class

--- InformationHubApp/src/com/example/informationhubapp/MainActivity.java
+++ InformationHubApp/src/com/example/informationhubapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -33,4 +34,5 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
 }
